126d8189716074d3b22c8f1cfe298ec4138d78af
dtiezzi <dtiezzi@usp.br>
2022-09-13 11:19:14 -0300

    dtiezzi init DB

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 import yahoo_fin.stock_info as si
+import sqlite3
 import os
 import time
 
 app = Flask(__name__)
 
+conn = sqlite3.connect('stock.db')
+print('CONNECTED!') if conn else print('ERR!')
 
 @app.route('/')
 def index():

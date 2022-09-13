c093b645445a678961d71089e70f24eb2815f18e
dtiezzi <dtiezzi@usp.br>
2022-09-13 13:45:00 -0300

    dtiezzi init flask app

--- app.py
+++ app.py
@@ -1,17 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect, request, session
 import yahoo_fin.stock_info as si
 import sqlite3
 import os
 import time
+from dotenv import load_dotenv
+from stock import Stock
 
 app = Flask(__name__)
 
-conn = sqlite3.connect('stock.db')
-print('CONNECTED!') if conn else print('ERR!')
+load_dotenv()
+app.secret_key = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def index():
-    return 'Hello App!'
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def user():
+    conn = sqlite3.connect('stock.db')
+    print('CONNECTED!') if conn else print('ERR!')
+    if request.method == 'POST':
+        session.user = 'dtiezzi'
+        return render_template('user.html', user=session.user)
+    else:
+        conn.close()
+        return render_template('index.html')
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    if request.method == 'POST':
+        session.clear()
+        return render_template('index.html')
+    else:
+        return render_template('user.html')
 
 if __name__ == '__main__':
     app.run()
---
+++ stock.sql
@@ -0,0 +1,37 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username CHAR(21)
+);
+
+CREATE TABLE tickers (
+    ticker CHAR(11) PRIMARY KEY NOT NULL
+);
+
+CREATE TABLE brokers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    st_name CHAR(11)
+);
+
+CREATE TABLE holding (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    ticker CHAR(11),
+    broker_id INTEGER,
+    purchase_value NUMERIC,
+    quant INTEGER,
+    purchase_dt DATE,
+    fg_active BOOL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (ticker) REFERENCES tickers(ticker),
+    FOREIGN KEY (broker_id) REFERENCES brokers(id)
+);
+
+CREATE TABLE sold (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    holding_id INTEGER,
+    sell_value NUMERIC,
+    quant INTEGER,
+    selling_dt DATE,
+    FOREIGN KEY (holding_id) REFERENCES holding(id)
+);
